5520c035c3bdc4b860a3aa17111300089637d5c0
Eddie Mapes <1999eddiem@gmail.com>
2023-06-19 12:35:02 -0500

    Changes to settings.py, added views, added url paths, added html templates

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.detail import ListView
+
+from inventory.models import *
 
 # Create your views here.
+
+def home(request):
+    return render(request, "inventory/home.html")
+
+class IngredientList(ListView):
+    model = Ingredient
+    template_name = inventory/ingredient_list.html
